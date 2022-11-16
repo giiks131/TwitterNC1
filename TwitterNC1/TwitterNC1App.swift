48f1d762536ac9d8decdf954b7b0a856dd094f57
Alex Sh <giiks131@gmail.com>
2022-11-16 13:37:47 +0100

    Initial Commit

---
+++ TwitterNC1/TwitterNC1App.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterNC1App.swift
+//  TwitterNC1
+//
+//  Created by Aleksandr Shapovalov on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterNC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
